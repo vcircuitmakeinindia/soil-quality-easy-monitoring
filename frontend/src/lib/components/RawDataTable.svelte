1191549de47e8eac2b0b0e9bebdceb19c5c88e74
Pranav <pranav.pooruli@gmail.com>
2024-11-21 21:44:02 +0530

    _prototyped_

---
+++ frontend/src/lib/components/RawDataTable.svelte
@@ -0,0 +1,49 @@
+<script>
+	let data = [
+		{
+			temperature: 43,
+			humidity: 99,
+			moisture: 300
+		}
+	];
+</script>
+
+<table id="rawdata-table">
+	<caption>Sensor clusters</caption>
+	<thead>
+		<tr>
+			<th>Cluster</th>
+			<th>Temperature&#x200a;/&#x200a;&#x2103;</th>
+			<th>Humidity&#x200a;/&#x200a;%</th>
+			<th>Moisture&#x200a;/&#x200a;a.u.</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each data as cluster, i}
+			<tr>
+				<td>{i + 1}</td>
+				<td>{cluster.temperature}</td>
+				<td>{cluster.humidity}</td>
+				<td>{cluster.moisture}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	#rawdata-table {
+		border-collapse: collapse;
+
+		font-family: "Open Sans", sans-serif;
+		font-family: "Parkinsans", sans-serif;
+	}
+
+	#rawdata-table th,
+	#rawdata-table td {
+		padding: 8px;
+
+		border: 1px solid black;
+
+		text-align: center;
+	}
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import VisualSection from "$lib/components/VisualSection.svelte";
+	import RawDataTable from "$lib/components/RawDataTable.svelte";
+</script>
+
+<div id="main-container">
+	<div id="visual-container">
+    <VisualSection />
+  </div>
+	<div id="rawdata-container">
+		<RawDataTable />
+	</div>
+</div>
+
+<style>
+	#main-container {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr;
+
+		height: 100%;
+	}
+
+	#main-container > * {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		width: 100%;
+		height: 100%;
+
+		outline: 1px solid black;
+	}
+
+	@media screen and (orientation: portrait) {
+		#main-container {
+			grid-template-columns: 1fr;
+			grid-template-rows: 1fr 1fr;
+		}
+	}
+
+  #visual-container {
+    background-image: url("$lib/assets/soil.png");
+    background-size: cover;
+  }
+</style>
