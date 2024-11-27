b7c5a864cd18e552e1ba3018a5e3500827d3cd5e
arx55 <164836470+arx55@users.noreply.github.com>
2024-11-27 21:59:47 +0530

    blood, sweat and arduino

--- backend/serial-fastapi/serial_fastapi/main.py
+++ backend/serial-fastapi/serial_fastapi/main.py
@@ -2,22 +2,24 @@ import serial
 from time import sleep
 import time
 import json
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-sleep(5)
+ser = serial.Serial("/dev/ttyACM0", 9600)
 
-
-@app.get("/sqm")
-async def sqm():
-    sleep(0.5)
-    ser = serial.Serial("/dev/ttyACM0", 9600)
+@app.route("/sqm")
+def sqm():
+    sleep(3)
     print(time.time())
     output = []
-    output.append(json.loads(ser.readline().decode("utf-8").strip()))
-    sleep(1)
-    output.append(json.loads(ser.readline().decode("utf-8").strip()))
-    sleep(1)
-    output.append(json.loads(ser.readline().decode("utf-8").strip()))
+    try:
+        output.append(json.loads(ser.readline().decode("utf-8").strip()))
+        sleep(1)
+        output.append(json.loads(ser.readline().decode("utf-8").strip()))
+        sleep(1)
+        output.append(json.loads(ser.readline().decode("utf-8").strip()))
+    except json.decoder.JSONDecodeError:
+        output = []
+    print(output)
     return {"data": output}
--- backend/sqm/sqm.ino
+++ backend/sqm/sqm.ino
@@ -1,14 +1,14 @@
 
 
-// Example testing sketch for various DHT humidity/temperature sensors written by ladyada
+// Example testing sketch for various DHT humidity/temperature clusters written by ladyada
 // REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+// - DHT cluster Library: https://github.com/adafruit/DHT-cluster-library
+// - Adafruit Unified cluster Lib: https://github.com/adafruit/Adafruit_cluster
 
 #include "DHT.h"
 
-#define DHHTPIN 7     // Digital pin connected to the DHT sensor
-#define DHXTPIN 8     // Digital pin connected to the DHT sensor
+#define DHHTPIN 7     // Digital pin connected to the DHT cluster
+#define DHXTPIN 8     // Digital pin connected to the DHT cluster
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
@@ -17,22 +17,28 @@
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 1 (on the left) of the cluster to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
 // to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+// Connect pin 2 of the cluster to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the cluster to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the cluster
 
-// Initialize DHT sensor.
+// Initialize DHT cluster.
 // Note that older versions of this library took an optional third parameter to
 // tweak the timings for faster processors.  This parameter is no longer needed
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dhht(DHHTPIN, DHTTYPE);
 DHT dhxt(DHXTPIN, DHTTYPE);
 
+int smh = A0;
+int smx = A1;
+int smy = A2;
+
 void setup() {
-  Serial.begin(115200);
+  pinMode(smh, INPUT);
+
+  Serial.begin(9600);
   dhht.begin();
   dhxt.begin();
 }
@@ -42,7 +48,7 @@ void loop() {
   delay(2000);
 
   // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // cluster readings may also be up to 2 seconds 'old' (its a very slow cluster)
   float hh = dhht.readHumidity();
   float hx = dhxt.readHumidity();
   // Read temperature as Celsius (the default)
@@ -54,11 +60,11 @@ void loop() {
 
   // Check if any reads failed and exit early (to try again).
 //  if (isnan(hh) || isnan(th) || isnan(fh) || isnan(hx) || isnan(tx) || isnan(fx)) {
-//    Serial.printlnln(F("Failed to read from DHT sensor!"));
+//    Serial.printlnln(F("Failed to read from DHT cluster!"));
 //    return;
 //  }
 
-  Serial.println("{sensor_no: 1, humidity: " + String(hh) + ", temperature: " + String(th) + "}");
-  Serial.println("{sensor_no: 2, humidity: " + String(hx) + ", temperature: " + String(tx) + "}");
-  Serial.println("{sensor_no: 3, humidity: " + String((hh + hx)/2) + ", temperature: " + String((th + tx)/2) + "}");
+  Serial.println("{\"cluster_no\": 1, \"humidity\": " + String(hh) + ", \"temperature\": " + String(th) + ", \"moisture\": " + analogRead(smh) + "}");
+  Serial.println("{\"cluster_no\": 2, \"humidity\": " + String(hx) + ", \"temperature\": " + String(tx) + ", \"moisture\": " + analogRead(smx) + "}");
+  Serial.println("{\"cluster_no\": 3, \"humidity\": " + String((hh + hx)/2) + ", \"temperature\": " + String((th + tx)/2) + ", \"moisture\": " + analogRead(smy) + "}");
 }
